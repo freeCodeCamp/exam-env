0cc0497ed9073e7e207f87be45714bb0c3a7d87a
Shaun Hamilton <shauhami020@gmail.com>
2024-12-04 16:05:17 +0200

    chore: update prisma config

--- prisma/post-generate.js
+++ prisma/post-generate.js
@@ -7,15 +7,20 @@ import jsonSchema from "./json-schema.json" with { type: "json" };
  * - https://github.com/valentinpalkovic/prisma-json-schema-generator/issues/1511
  */
 async function main() {
-
   jsonSchema.definitions.EnvExam.required.push("config", "questionSets");
   jsonSchema.definitions.EnvConfig.required.push("tags", "questionSets");
-  jsonSchema.definitions.EnvMultipleChoiceQuestion.required.push("audio", "answers");
-  jsonSchema.definitions.EnvAudio.required.push("captions","startTimeInMS","endTimeInMS");
-  jsonSchema.definitions.EnvQuestionSet.required.push("context","questions");
+  jsonSchema.definitions.EnvMultipleChoiceQuestion.required.push(
+    "audio",
+    "answers"
+  );
+  jsonSchema.definitions.EnvQuestionSet.required.push("context", "questions");
   jsonSchema.definitions.EnvExamAttempt.required.push("questionSets");
-  jsonSchema.definitions.EnvMultipleChoiceQuestionAttempt.required.splice(jsonSchema.definitions.EnvMultipleChoiceQuestionAttempt.required.indexOf("submissionTimeInMS"),1)
-
+  jsonSchema.definitions.EnvMultipleChoiceQuestionAttempt.required.splice(
+    jsonSchema.definitions.EnvMultipleChoiceQuestionAttempt.required.indexOf(
+      "submissionTimeInMS"
+    ),
+    1
+  );
 
   await writeFile(
     "./prisma/json-schema.json",
