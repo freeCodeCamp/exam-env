11439459e9bfbc2545538e4e9cff30ffcafbf1b1
Shaun Hamilton <shauhami020@gmail.com>
2025-03-11 13:39:38 +0200

    fix: add custom audio player

---
+++ src/components/audio-player.tsx
@@ -0,0 +1,121 @@
+import { TriangleUpIcon } from "@chakra-ui/icons";
+import {
+  Box,
+  Button,
+  Flex,
+  Slider,
+  SliderFilledTrack,
+  SliderThumb,
+  SliderTrack,
+  Text,
+} from "@chakra-ui/react";
+import { useEffect, useRef, useState } from "react";
+import { FullQuestion } from "../utils/types";
+
+interface AudioPlayerProps {
+  fullQuestion: FullQuestion;
+}
+
+export function AudioPlayer({ fullQuestion }: AudioPlayerProps) {
+  const audioRef = useRef<HTMLAudioElement | null>(null);
+
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [progress, setProgress] = useState(0);
+  const [duration, setDuration] = useState(0);
+
+  useEffect(() => {
+    if (!fullQuestion.audio) {
+      return;
+    }
+    audioRef.current = new Audio(fullQuestion.audio.url);
+
+    audioRef.current.onloadedmetadata = onLoadedMetadata;
+    audioRef.current.ontimeupdate = onTimeUpdate;
+    audioRef.current.onpause = onPause;
+    audioRef.current.onplay = onPlay;
+
+    return () => {
+      if (!audioRef.current) {
+        return;
+      }
+      audioRef.current.pause();
+      audioRef.current = null;
+    };
+  }, [fullQuestion]);
+
+  function onLoadedMetadata() {
+    if (!audioRef.current) {
+      return;
+    }
+    setDuration(audioRef.current.duration);
+  }
+
+  function onTimeUpdate() {
+    if (!audioRef.current) {
+      return;
+    }
+    setProgress(audioRef.current.currentTime);
+  }
+
+  function onPause() {
+    setIsPlaying(false);
+  }
+
+  function onPlay() {
+    setIsPlaying(true);
+  }
+
+  const togglePlay = () => {
+    if (isPlaying) {
+      audioRef.current?.pause();
+    } else {
+      audioRef.current?.play();
+    }
+  };
+
+  function handleSeek(value: number) {
+    if (!audioRef.current) {
+      return;
+    }
+    audioRef.current.currentTime = value;
+    setProgress(value);
+  }
+
+  function formatTime(time: number): string {
+    const minutes = Math.floor(time / 60);
+    const seconds = Math.floor(time % 60);
+    return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+  }
+
+  if (!audioRef.current || !fullQuestion.audio) {
+    return null;
+  }
+
+  return (
+    <Box>
+      <Flex direction="column">
+        <Flex alignItems="center">
+          <Button onClick={togglePlay}>
+            {isPlaying ? "=" : <TriangleUpIcon style={{ rotate: "90deg" }} />}
+          </Button>
+          <Text ml={2}>
+            {formatTime(audioRef.current.currentTime)} / {formatTime(duration)}
+          </Text>
+        </Flex>
+
+        <Slider
+          aria-label="slider-ex-1"
+          min={0}
+          max={duration}
+          value={progress}
+          onChange={(val) => handleSeek(val)}
+        >
+          <SliderTrack>
+            <SliderFilledTrack />
+          </SliderTrack>
+          <SliderThumb />
+        </Slider>
+      </Flex>
+    </Box>
+  );
+}
--- src/components/question-set-form.tsx
+++ src/components/question-set-form.tsx
@@ -1,11 +1,12 @@
-import { QuizQuestion } from "@freecodecamp/ui";
+import { UseMutationResult } from "@tanstack/react-query";
 import { Box, Text, Spacer } from "@chakra-ui/react";
-import { useEffect, useState, useRef } from "react";
+import { QuizQuestion } from "@freecodecamp/ui";
 import Markdown from "markdown-to-jsx";
+import { useEffect } from "react";
 
 import { Answers, FullQuestion, UserExamAttempt } from "../utils/types";
 import { ButtonLoading } from "./button-loading";
-import { UseMutationResult } from "@tanstack/react-query";
+import { AudioPlayer } from "./audio-player";
 
 type QuestionTypeFormProps = {
   fullQuestion: FullQuestion;
@@ -31,23 +32,6 @@ export function QuestionSetForm({
   setNewSelectedAnswers,
   examAttempt,
 }: QuestionTypeFormProps) {
-  const audioRef = useRef<HTMLAudioElement | null>(null);
-  const [audioSource, setAudioSource] = useState("");
-  function resetAudio() {
-    if (audioRef.current) {
-      // This is to prevent a stupid UI bug (all Shaun's fault)
-      audioRef.current.load();
-      audioRef.current
-        .play()
-        .then(() => {
-          if (audioRef.current) {
-            audioRef.current.pause();
-          }
-        })
-        .catch(console.error);
-    }
-  }
-
   useEffect(() => {
     setNewSelectedAnswers(
       fullQuestion.answers
@@ -58,17 +42,7 @@ export function QuestionSetForm({
         )
         .map((a) => a.id)
     );
-    if (fullQuestion.audio) {
-      setAudioSource(fullQuestion.audio.url);
-    }
-    return () => {
-      // https://github.com/freeCodeCamp/exam-env/issues/21
-      // This is done during unmount to ensure the audio pauses after submission.
-      resetAudio();
-    };
   }, [fullQuestion]);
-
-  useEffect(resetAudio, [audioSource]);
 
   return (
     <>
@@ -78,15 +52,11 @@ export function QuestionSetForm({
           <Markdown>{fullQuestion.questionSet.context}</Markdown>
         </>
       )}
-      {fullQuestion.audio?.url && (
+      {fullQuestion.audio && (
         <Box mb={"2em"} mt={"2em"}>
           <Text>Please listen to the following audio fragment:</Text>
-          <audio
-            controls
-            controlsList="nodownload"
-            ref={audioRef}
-            src={audioSource}
-          ></audio>
+          {/* NOTE: `fullQuestion` is passed to cause the whole component to rerender - correctly resetting the audio */}
+          <AudioPlayer fullQuestion={fullQuestion} />
         </Box>
       )}
       <Text fontWeight={"bold"}>Question</Text>
