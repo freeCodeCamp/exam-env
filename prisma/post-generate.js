4c6f11329238f935158a862ee81a4b54419c0adc
Shaun Hamilton <shauhami020@gmail.com>
2025-06-27 13:13:57 +0200

    chore: update client and backend deps (#41)

--- prisma/post-generate.js
+++ prisma/post-generate.js
@@ -31,13 +31,19 @@ async function main() {
 }
 
 async function openApi() {
-  const url = "https://test-api.freecodecamp.dev/documentation/json";
+  // Link to locally running instance of `freeCodeCamp/freeCodeCamp/api`
+  const url = "http://127.0.0.1:3000/documentation/json";
 
-  const ast = await openapiTS(new URL(url));
-  const contents = astToString(ast);
-
-  // (optional) write to file
-  await writeFile("./prisma/api-schema.ts", contents);
+  try {
+    const ast = await openapiTS(new URL(url));
+    const contents = astToString(ast);
+  
+    // (optional) write to file
+    await writeFile("./prisma/api-schema.ts", contents);
+  } catch(e) {
+    console.error("Unable to get openapi spec for freeCodeCamp API");
+    throw e;
+  }
 }
 
 await main();
