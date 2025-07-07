2574f830e7deddbea3ea08abb1759b30f02ae7ea
Shaun Hamilton <shauhami020@gmail.com>
2025-07-07 14:11:45 +0200

    feat(client): default prisma theme

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(async () => ({
     prism({
       languages: "all",
       plugins: ["line-numbers"],
-      theme: "okaidia",
+      theme: "default",
       css: true,
     }),
   ],
