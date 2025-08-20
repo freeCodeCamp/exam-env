a627b84cc5b07b168a7823cf7094bbf5b26c71d3
Shaun Hamilton <shauhami020@gmail.com>
2025-08-20 13:36:31 +0200

    chore: update types and deps (#47)

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,13 +1,16 @@
 import type * as Prisma from "@prisma/client";
 
-export type UserExam = Omit<EnvExam, "questionSets" | "config" | "id"> & {
-  config: Omit<EnvExam["config"], "tags" | "questionSets">;
-  questionSets: (Omit<EnvQuestionSet, "questions"> & {
+export type UserExam = Omit<
+  ExamEnvironmentExam,
+  "questionSets" | "config" | "id"
+> & {
+  config: Omit<ExamEnvironmentExam["config"], "tags" | "questionSets">;
+  questionSets: (Omit<ExamEnvironmentQuestionSet, "questions"> & {
     questions: (Omit<
-      EnvMultipleChoiceQuestion,
+      ExamEnvironmentMultipleChoiceQuestion,
       "answers" | "tags" | "deprecated" | "submissionTimeInMS"
     > & {
-      answers: Omit<EnvAnswer, "isCorrect">[];
+      answers: Omit<ExamEnvironmentAnswer, "isCorrect">[];
     })[];
   })[];
 } & { generatedExamId: string; examId: string };
@@ -20,28 +23,33 @@ export type Answers =
   UserExam["questionSets"][number]["questions"][number]["answers"];
 
 export type UserExamAttempt = Omit<
-  EnvExamAttempt,
+  ExamEnvironmentExamAttempt,
   "submissionTimeInMS" | "questionSets"
 > & {
-  questionSets: (Omit<EnvQuestionSetAttempt, "questions"> & {
-    questions: Omit<EnvMultipleChoiceQuestionAttempt, "submissionTimeInMS">[];
+  questionSets: (Omit<ExamEnvironmentQuestionSetAttempt, "questions"> & {
+    questions: Omit<
+      ExamEnvironmentMultipleChoiceQuestionAttempt,
+      "submissionTimeInMS"
+    >[];
   })[];
 };
 
-export type EnvExam = Prisma.EnvExam;
-export type EnvAnswer = Prisma.EnvAnswer;
-export type EnvMultipleChoiceQuestion = Prisma.EnvMultipleChoiceQuestion;
-export const EnvQuestionType = {
+export type ExamEnvironmentExam = Prisma.ExamEnvironmentExam;
+export type ExamEnvironmentAnswer = Prisma.ExamEnvironmentAnswer;
+export type ExamEnvironmentMultipleChoiceQuestion =
+  Prisma.ExamEnvironmentMultipleChoiceQuestion;
+export const ExamEnvironmentQuestionType = {
   MultipleChoice: "MultipleChoice",
   Dialogue: "Dialogue",
 } as const;
-export type EnvQT = keyof typeof EnvQuestionType;
-export type EnvQuestionSet = Prisma.EnvQuestionSet;
-export type EnvConfig = Prisma.EnvConfig;
-type EnvExamAttempt = Prisma.EnvExamAttempt;
-export type EnvQuestionSetAttempt = Prisma.EnvQuestionSetAttempt;
-export type EnvMultipleChoiceQuestionAttempt =
-  Prisma.EnvMultipleChoiceQuestionAttempt;
+export type ExamEnvironmentQT = keyof typeof ExamEnvironmentQuestionType;
+export type ExamEnvironmentQuestionSet = Prisma.ExamEnvironmentQuestionSet;
+export type ExamEnvironmentConfig = Prisma.ExamEnvironmentConfig;
+type ExamEnvironmentExamAttempt = Prisma.ExamEnvironmentExamAttempt;
+export type ExamEnvironmentQuestionSetAttempt =
+  Prisma.ExamEnvironmentQuestionSetAttempt;
+export type ExamEnvironmentMultipleChoiceQuestionAttempt =
+  Prisma.ExamEnvironmentMultipleChoiceQuestionAttempt;
 
 export type UnrecoverableError = {
   source: String;
